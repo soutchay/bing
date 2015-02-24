e7a30e94254c8e350415abe3a8f690d1d321f8e5
soutchay <chungvus@gmail.com>
2015-02-24 11:50:17 -0800

    setup superagent

--- server.js
+++ server.js
@@ -3,23 +3,25 @@ var app = express();
 var path = require('path');
 var http = require('http');
 var parseString = require('xml2js').parseString;
-
+var superagent = require('superagent');
 //Views
 app.set("view engine", 'ejs');
 
 var router = express.Router();
 
-// https://api.datamarket.azure.com/Bing/Search/Web?Query='cats'&$format=json
-var acctKey = 'CLir7B5jwG5r2PPBhwyGxia7tsVyggkcyHkgcdvRPSs';
-var rootUri = 'https://api.datamarket.azure.com/Bing/Search/Web';
+// https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27cats%27&$format=json
+// acctkey = hKA0fi33iYFVxY4bqaAE6EOtsjWt9Uluu556ZpdU9Sc
+// var acctKey = 'CLir7B5jwG5r2PPBhwyGxia7tsVyggkcyHkgcdvRPSs';
+var acctKey = 'hKA0fi33iYFVxY4bqaAE6EOtsjWt9Uluu556ZpdU9Sc';
+var rootUri = 'https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%';
 var auth    = new Buffer([ acctKey, acctKey ].join(':')).toString('base64');
 var request = require('request').defaults({
   headers : {
-    'Authorization' : 'Basic ' + auth
+    'Authorization' : 'Basic ' + "OmhLQTBmaTMzaVlGVnhZNGJxYUFFNkVPdHNqV3Q5VWx1dTU1NlpwZFU5U2M"
   }
 });
 
-
+console.log(auth);
 //perform a query:
 app.get('/search', function(req, res) {
   // var service_op  = req.body.service_op;
@@ -29,7 +31,7 @@ app.get('/search', function(req, res) {
     url : rootUri + '/',
     qs  : {
       $format : 'json',
-      Query   : "'" + query + "'",
+      Query   : "%27" + query + "%27",
     }
   }, function(err, response, body) {
     if (err)
@@ -44,10 +46,10 @@ app.get('/search', function(req, res) {
 app.get('/bing', function(req, res){
   var options = {
     host : 'api.datamarket.azure.com',
-    path : "/Bing/Search/Web?Query='cats'&$format=json",
+    path : "/Bing/Search/Web?Query=%27cats%27&$format=json",
     port : 80,
     method : 'GET'
-  }
+  };
 
   var request = http.request(options, function(response){
     var body = "";
@@ -69,10 +71,26 @@ app.use(express.static(path.join(__dirname, '/views')));
 app.use(express.static(__dirname + '/app'));
 app.use(express.static(__dirname + '/bower_components'));
 
+
+//set up superagent
+var query = "'cats'";
+superagent.get("https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27cats%27&$format=json")
+  .query({Query: query})
+  .query({$format: "json"})
+  .set('Authorization', 'Basic OmhLQTBmaTMzaVlGVnhZNGJxYUFFNkVPdHNqV3Q5VWx1dTU1NlpwZFU5U2M=')
+  .end(function(res){
+    if (res.ok){
+      console.log(res.body);
+    }
+    else{
+      console.log('error');
+    }
+  });
+
 //Set up route for opening page
 router.route('/')
     .get(function(req, res){
-        response.status(200).render("index")
+        response.status(200).render("index");
     });
 
 app.use(router);
